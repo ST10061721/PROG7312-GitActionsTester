ce0867feb518832feebb2f9c55cc40095bca0b68
Dillon Wernich <st10061721@vcconnect.edu.za>
2024-09-09 10:21:25 +0200

    Commit

---
+++ GitActionsTester/GitActionsTester/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitActionsTester
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing");
+            Console.WriteLine("BACKUP");
+            Console.ReadLine();
+        }
+    }
+}
